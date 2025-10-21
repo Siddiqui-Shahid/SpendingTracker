36619029ae7c24f528b975b1245ecf28fff6316a
Shahid Siddiqui <71084660+Siddiqui-Shahid@users.noreply.github.com>
2025-10-21 18:03:41 +0530

    Simplify TabsManager layout by removing floating action button and bottom navigation bar

--- spending_tracker/lib/Screens/tabs_manager.dart
+++ spending_tracker/lib/Screens/tabs_manager.dart
@@ -137,42 +137,7 @@ class _TabsManager extends State<TabsManager> {
     }
     return DefaultTabController(
       length: 2,
-      child: Scaffold(
-        body: _screens[_selectedIndex],
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Navigator.of(context).push(_createRoute());
-          },
-          tooltip: "Create",
-          elevation: 0.0,
-          child: const Icon(Icons.add),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        bottomNavigationBar: BottomNavigationBar(
-          elevation: 4,
-          selectedIconTheme: const IconThemeData(
-            color: Colors.blueAccent,
-            size: 30,
-          ),
-
-          showSelectedLabels: true,
-          selectedFontSize: 13,
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.account_balance_wallet_rounded, size: 25),
-              label: "Home",
-              tooltip: "Home Screen",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.account_balance_rounded, size: 25),
-              label: "Account",
-              tooltip: "Account Section",
-            ),
-          ],
-          currentIndex: _selectedIndex,
-          onTap: _onTappedItem,
-        ),
-      ),
+      child: Scaffold(body: _screens[_selectedIndex]),
     );
   }
 }
